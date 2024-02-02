7dbc68fdac66c3fa858df8f3d22343e9627c7de2
arkansas-owasp <136471015+arkansas-owasp@users.noreply.github.com>
2024-02-02 17:04:55 -0600

    Double the fun for a new hash
    // adding comments did NOT change the hash so I added the hello world function a 2nd time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     println!("Hello, world!");
+    println!("Hello, world!");
 }
 // adding comments to see if it yields a new hash 20240202Fri1627 AL
+// adding comments did NOT change the hash so I added the hello world function a 2nd time
