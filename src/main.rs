b474faca79dfb166aba02d78a73b2e6186d972ab
arkansas-owasp <136471015+arkansas-owasp@users.noreply.github.com>
2024-02-02 17:08:53 -0600

    Added EICAR
    // added the EICAR test string 20240202Fri1708 X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("Hello, world!");
     println!("Hello, world!");
+    println!("X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
 }
 // adding comments to see if it yields a new hash 20240202Fri1627 AL
 // adding comments did NOT change the hash so I added the hello world function a 2nd time
+// added the EICAR test string 20240202Fri1708 X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*
