8d0dd21c5bc5be8e75989ebe957fa2de6caf59c6
arkansas-owasp <136471015+arkansas-owasp@users.noreply.github.com>
2024-02-04 18:42:24 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
     println!("Hello, world!");
     println!("Hello, world!");
-    println!("'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'");
+    // println!("'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'");
+    println!(r"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
 }
 // adding comments to see if it yields a new hash 20240202Fri1627 AL
 // adding comments did NOT change the hash so I added the hello world function a 2nd time
 // added the EICAR test string 20240202Fri1708 X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*
 // bracketed the EICAR string with single quotes since I got an odd Unrecognized escape error
 // println!("'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'");
+// After 2 failures I read the Rust help provided w/the error and did this 
+// println!(r"'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'");
